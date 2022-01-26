23529f49cb3334eb280e4b6f717a1403456ea03b
njajkes <naikes.nikolait@yandex.ru>
2022-01-26 21:06:50 +0300

    v.0.2.1

--- services/callSendedJoinRequests.ts
+++ services/callSendedJoinRequests.ts
@@ -3,22 +3,27 @@ import { Update } from "telegraf/typings/core/types/typegram";
 import { groupModel } from "../models/groups";
 import { userModel } from "../models/users";
 
+// regular notifications for group admins about new join requests  
 export async function callSendedJoinRequests(bot:Telegraf<Context<Update>>) {
   const candidates = await userModel.find({role: "sended"})
   const admins: object = {}
 
+  // 
   candidates.forEach(async (candidate) => {
-    const group = await groupModel.findOne({_id: candidate.groupID})
+    const group = await groupModel.findOne({groupName: candidate.groupName})
     if (!admins[group.adminID]) {
-      admins[group.adminID] = [candidate.uid]
+      admins[group.adminID] = [candidate.username]
     } else {
-      admins[group.adminID].push(candidate.uid)
+      admins[group.adminID].push(candidate.username)
     }
     candidate.role = "pending"
     await candidate.save()
   })
   
+  const messages = new Map()
   for (let k of Object.getOwnPropertyNames(admins)) {
-
+    // todo: add message-maker
   }
+
+  // todo: add mailing
 }
